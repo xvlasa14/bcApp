6638d63b6f6d5d3b9510dddbbfd340dfc56ebc52
Andrea Chimenti <andre.chimenti@gmail.com>
2023-12-17 21:48:24 +0100

    Added information about logged user

--- salvia/src/utils/user.ts
+++ salvia/src/utils/user.ts
@@ -1,99 +1,110 @@
 import formurlencoded from "form-urlencoded";
 async function getUsers() {
-	const reply = await fetch(import.meta.env.VITE_DB_URL + "/users", {
-		method: "GET",
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-type": "application/json",
-			Authorization: "Bearer " + import.meta.env.VITE_TOKEN,
-		},
-	});
+    const reply = await fetch(import.meta.env.VITE_DB_URL + "/users", {
+        method: "GET",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json",
+            Authorization: "Bearer " + import.meta.env.VITE_TOKEN,
+        },
+    });
 
-	return reply.json();
+    return reply.json();
 }
 
 async function getUser(id: number) {
-	const reply = await fetch(import.meta.env.VITE_DB_URL + "/users/" + id, {
-		method: "GET",
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-type": "application/json",
-			Authorization: "Bearer " + import.meta.env.VITE_TOKEN,
-		},
-	});
+    const reply = await fetch(import.meta.env.VITE_DB_URL + "/users/" + id, {
+        method: "GET",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + import.meta.env.VITE_TOKEN,
+        },
+    });
 
-	return reply.json();
+    return reply.json();
 }
 
 async function createUser(
-	username: string,
-	email: string,
-	password: string,
-	firstName: string,
-	lastName: string,
-	role: string
+    username: string,
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    role: string
 ) {
-	const u = {
-		username: username,
-		email: email,
-		password: password,
-		first_name: firstName,
-		last_name: lastName,
-		role_id: role,
-	};
+    const u = {
+        "username": username,
+        "email": email,
+        "password": password,
+        "first_name": firstName,
+        "last_name": lastName,
+        "role_id": role
+    }
 
-	const reply = await fetch(import.meta.env.VITE_DB_URL + "/users", {
-		method: "POST",
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-type": "application/json",
-			Authorization: "Bearer " + import.meta.env.VITE_TOKEN,
-		},
-		body: JSON.stringify(u),
-	});
+    const reply = await fetch(import.meta.env.VITE_DB_URL + "/users", {
+        method: "POST",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + import.meta.env.VITE_TOKEN
+        },
+        body: JSON.stringify(u)
+    });
 
-	return await reply.json();
+    return await reply.json();
 }
 
 async function deleteUser(id: number) {
-	const reply = await fetch(import.meta.env.VITE_DB_URL + "/users/" + id, {
-		method: "DELETE",
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-type": "application/json",
-			Authorization: "Bearer " + import.meta.env.VITE_TOKEN,
-		},
-	});
+    const reply = await fetch(import.meta.env.VITE_DB_URL + "/users/" + id, {
+        method: "DELETE",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + import.meta.env.VITE_TOKEN,
+        },
+    });
 
-	return reply;
+    return reply;
 }
 
+
+/**
+ * Log in a user with the provided username and password. Set the token in the environment variable.
+ * 
+ * 
+ * @param {string} username - The username of the user.
+ * @param {string} password - The password of the user.
+ * @returns {Promise<void>} - A promise that resolves when the user is logged in successfully.
+ */
 async function loginUser(username: string, password: string) {
-	const reply = await fetch(import.meta.env.VITE_DB_URL + "/auth/token", {
-		method: "POST",
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-type": "application/x-www-form-urlencoded",
-		},
-		body: formurlencoded({ username: username, password: password }),
-	});
+    const reply = await fetch(import.meta.env.VITE_DB_URL + "/auth/token", {
+        method: "POST",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: formurlencoded({ username: username, password: password }),
+    });
 
-	if (reply.ok) {
-		import.meta.env.VITE_TOKEN = (await reply.json()).access_token;
-		console.log(import.meta.env.VITE_TOKEN);
-	}
+    if (reply.ok) {
+        import.meta.env.VITE_TOKEN = (await reply.json()).access_token;
+        console.log(import.meta.env.VITE_TOKEN);
+    } else {
+        throw new Error('Login failed');
+    }
 }
 
-async function authUser() {
-	const reply = await fetch(import.meta.env.VITE_DB_URL + "/auth/me", {
-		method: "GET",
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-type": "application/json",
-			Authorization: "Bearer " + import.meta.env.VITE_TOKEN,
-		},
-	});
-	console.log(await reply.json());
+async function authUser(): Promise<Response> {
+    const reply = await fetch(import.meta.env.VITE_DB_URL + "/auth/me", {
+        method: "GET",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + import.meta.env.VITE_TOKEN,
+        },
+    });
+    return reply;
 }
 
 export { authUser, loginUser, deleteUser, createUser, getUser, getUsers };
--- salvia/src/views/App.vue
+++ salvia/src/views/App.vue
@@ -1,12 +1,36 @@
 <template id="app">
-    <h1>ROUTING temp</h1>
-    <p>
-        <router-link to="/">HOME</router-link> //
-        <router-link to="/login">LOGIN</router-link> //
-        <router-link to="/user">USER profile</router-link> //
-        <router-link to="/patient">PATIENT profile</router-link> //
-        <router-link to="/tests">TESTs overview</router-link>
-    </p>
-    <p>=======================================================</p>
+  <p>Logged user: {{ logged_user }}</p>
+  <h1>ROUTING temp</h1>
+  <p>
+    <router-link to="/">HOME</router-link> //
+    <router-link to="/login">LOGIN</router-link> //
+    <router-link to="/user">USER profile</router-link> //
+    <router-link to="/patient">PATIENT profile</router-link> //
+    <router-link to="/tests">TESTs overview</router-link>
+  </p>
+  <p>=======================================================</p>
   <router-view></router-view>
 </template>
+
+
+<script lang="ts">
+import { authUser } from "../utils/user";
+
+export default {
+  data() {
+    return {
+      logged_user: "",
+    };
+  },
+  async mounted() {
+    const response = await authUser();
+    if (response.ok) {
+      const json = await response.json();
+      this.logged_user = json["username"];
+    } else {
+      this.logged_user = "not logged in";
+    }
+  },
+}
+
+</script>
